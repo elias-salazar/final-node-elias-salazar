a676f4fecf09313a26279740aabaf15fcffc2c41
elias-salazar <eliotssalazarcampos@gmail.com>
2022-12-01 19:38:59 -0300

    ultimo

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const db = require("./utils/database");
+const morgan = require("morgan");
+const cors = require("cors");
 const handleError = require("./middlewares/error.middleware");
 const initModels = require("./models/initModels");
 const { userRoutes, authRoutes, productRoutes } = require("./routes");
@@ -7,7 +9,8 @@ const transporter = require("./utils/mailer");
 const app = express();
 
 app.use(express.json());
-
+app.use(morgan("dev"));
+app.use(cors());
 initModels();
 db.authenticate()
   .then(() => console.log("Autenticacion exitosa"))
